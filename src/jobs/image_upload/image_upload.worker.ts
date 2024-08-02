2f5831b1c930326d9f685d3fce4ef2dea7d30f76
Ikem Nomso <ikemnomso@gmail.com>
2024-08-02 14:40:37 +0100

    refactor(image upload worker): extract code from switch cases into seperate functions

--- src/jobs/image_upload/image_upload.worker.ts
+++ src/jobs/image_upload/image_upload.worker.ts
@@ -15,54 +15,62 @@ const uploadToCloudinary = async (job: Job) => {
 
 	const { image, images, userId, eventId } = job.data;
 
+	const uploadSingleImage = async () => {
+		try {
+			const filter = { _id: userId };
+
+			// upload file to cloudinary
+			const cloudinaryResponse = await cloudinary.uploader.upload(image);
+
+			//delete file from directory
+			await fs.unlink(image);
+
+			const update = { avatar: cloudinaryResponse.secure_url };
+
+			await UserModel.findOneAndUpdate(filter, update, {
+				new: true,
+			});
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const uploadMultipleImages = async () => {
+		try {
+			const filter = { _id: eventId };
+
+			// upload files to cloudinary
+			const imagesToUpload = images.map(async (image: string) => {
+				const result = await cloudinary.uploader.upload(image);
+				return result;
+			});
+
+			const uploads = await Promise.all(imagesToUpload);
+			const cloudinaryUploadUrls: string[] = uploads.map(
+				(upload) => upload.secure_url
+			);
+
+			//delete files from directory
+			images.forEach(async (image: string) => {
+				await fs.unlink(image);
+			});
+
+			const update = { images: [...cloudinaryUploadUrls] };
+
+			await EventModel.findOneAndUpdate(filter, update, {
+				new: true,
+			});
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	switch (job.name) {
 		case jobNames.singleUpload:
-			try {
-				const filter = { _id: userId };
-
-				// upload file to cloudinary
-				const cloudinaryResponse = await cloudinary.uploader.upload(image);
-
-				//delete file from directory
-				await fs.unlink(image);
-
-				const update = { avatar: cloudinaryResponse.secure_url };
-
-				await UserModel.findOneAndUpdate(filter, update, {
-					new: true,
-				});
-			} catch (error) {
-				console.error(error);
-			}
+			uploadSingleImage();
 			break;
 		case jobNames.multipleUpload:
-			try {
-				const filter = { _id: eventId };
-
-				// upload files to cloudinary
-				const imagesToUpload = images.map(async (image: string) => {
-					const result = await cloudinary.uploader.upload(image);
-					return result;
-				});
-
-				const uploads = await Promise.all(imagesToUpload);
-				const cloudinaryUploadUrls: string[] = uploads.map(
-					(upload) => upload.secure_url
-				);
-
-				//delete files from directory
-				images.forEach(async (image: string) => {
-					await fs.unlink(image);
-				});
-
-				const update = { images: [...cloudinaryUploadUrls] };
-
-				await EventModel.findOneAndUpdate(filter, update, {
-					new: true,
-				});
-			} catch (error) {
-				console.error(error);
-			}
+			uploadMultipleImages();
 			break;
 		default:
 			return;
