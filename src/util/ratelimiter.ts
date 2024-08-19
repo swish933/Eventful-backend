cec27b42914275e491e8ce8929431af5e3d2ba60
Ikem Nomso <ikemnomso@gmail.com>
2024-08-19 13:02:20 +0100

    import verifyToken fn

--- src/util/ratelimiter.ts
+++ src/util/ratelimiter.ts
@@ -1,5 +1,6 @@
 import { rateLimit } from "express-rate-limit";
 import { Request } from "express";
+import { verifyToken } from "../middleware/access-control.middleware";
 
 export const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
